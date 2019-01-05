5d0e481581e5bc238bbaebb5bcb29eac5469dab5
Sotatek-TinDuong <tin.duong@sotatek.com>
2019-01-05 09:51:28 +0700

    add icon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Link} from 'react-router-dom';
 import './resources/assets/sass/app.scss';
+import './resources/assets/icomoon/icon.scss'
+import SearchBox from './resources/assets/components/SearchBox';
 
 class App extends Component {
   render() {
@@ -10,6 +12,7 @@ class App extends Component {
           <div id="header">
             <div className="header__container w-960">
               <div className="logo"><Link to="/"><img src="./images/heart-logo.svg" alt="Couple Lock" /><span>LoveLock</span></Link></div>
+              <SearchBox />
             </div>
           </div>
           <div id="main"></div>
---
+++ src/resources/assets/components/SearchBox.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchBox extends Component {
+  render() {
+    return (
+      <div className="search_box">
+          <input type="text" name="" placeholder="Search" />
+      </div>
+    );
+  }
+}
+
+export default SearchBox;
