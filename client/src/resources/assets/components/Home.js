92b5c596362444445ab281bf077cbda7af2126f6
Sotatek-TinDuong <tin.duong@sotatek.com>
2019-01-10 11:11:49 +0700

    places

--- client/src/resources/assets/components/Home.js
+++ client/src/resources/assets/components/Home.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import MainContent from './MainContent';
+import LocationSearchInput from './Places';
 
 class Home extends Component {
   render() {
     return (
       <div className="home_page">
         <Header />
+        <LocationSearchInput />
         <MainContent />
       </div>
     );
