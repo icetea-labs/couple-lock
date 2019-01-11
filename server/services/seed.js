e5b5f9d1590d2f4804d0d71e251071a8e5695956
Truong Hong Thi <thi1981@gmail.com>
2019-01-11 10:50:53 +0700

    Update server/services/seed.js

--- server/services/seed.js
+++ server/services/seed.js
@@ -25,44 +25,40 @@ seeder.propose = [
         chained: false,
         visibility: 1, //1: listed, 2: public, 3: private
         sender: "sotatek",
+        s_timestamp: Date.now(),
+        s_visibilitySuggestion: 1,
+        s_receiverCanChangeVisibility: 1, // 1: none, 2: reduceOnly, 3: all
+        s_message: "I will alway love you, no matter what God bring down to us!",
+        s_attachments: [
+            {
+                type: 'photo',
+                url: "https://dq1eylutsoz4u.cloudfront.net/2018/01/25162144/relationship-tips-happy-couples.jpg",
+                caption: "Remember where this photo take?",
+                timestamp: Date.now(),
+                location: {
+                    name: "Mongolia",
+                    lat: 0,
+                    long: 0
+                }
+            }
+        ],
         receiver: "tradatech",
-        sending: {
-            timestamp: Date.now(),
-            visibilitySuggestion: 1,
-            receiverCanChangeVisibility: 1, // 1: none, 2: reduceOnly, 3: all
-            message: "I will alway love you, no matter what God bring down to us!",
-            attachments: [
-                {
-                    type: 'photo',
-                    url: "https://dq1eylutsoz4u.cloudfront.net/2018/01/25162144/relationship-tips-happy-couples.jpg",
-                    caption: "Remember where this photo take?",
-                    timestamp: Date.now(),
-                    location: {
-                        name: "Mongolia",
-                        lat: 0,
-                        long: 0
-                    }
+        r_react: 1, //null, 1: accepted, 2: denied
+        r_timestamp: Date.now(),
+        r_message: "OK, fine!",
+        r_attachments: [
+            {
+                type: 'photo',
+                url: "https://boygeniusreport.files.wordpress.com/2016/11/puppy-dog.jpg",
+                caption: "My dog likes that :D",
+                timestamp: Date.now(),
+                location: {
+                    name: "The Moon",
+                    lat: 10,
+                    long: 10
                 }
-            ],
-        },
-        receiving: {
-            react: 1, //null, 1: accepted, 2: denied
-            timestamp: Date.now(),
-            message: "OK, fine!",
-            attachments: [
-                {
-                    type: 'photo',
-                    url: "https://boygeniusreport.files.wordpress.com/2016/11/puppy-dog.jpg",
-                    caption: "My dog likes that :D",
-                    timestamp: Date.now(),
-                    location: {
-                        name: "The Moon",
-                        lat: 10,
-                        long: 10
-                    }
-                }
-            ]
-        }
+            }
+        ]
     }
 ];
 
