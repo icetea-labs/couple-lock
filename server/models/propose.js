2b2b2ffdf9c4b9b1ea41076fbeed318a2aec6458
Truong Hong Thi <thi1981@gmail.com>
2019-01-18 01:21:54 +0700

    Update server/models/propose.js

--- server/models/propose.js
+++ server/models/propose.js
@@ -34,3 +34,17 @@ exports.insert = async (data, cb) => {
 
     return store.insert(data.id, data, cb);
 }
+
+exports.update = (id, newProps, cb) => {
+    if (newProps.id && newProps.id !== id) {
+        return promise.cbOrFail("Changing key is not allowed")
+    }
+    if (newProps.sender) {
+        return promise.cbOrFail("Changing sender is not allowed")
+    }
+    if (newProps.receiver) {
+        return promise.cbOrFail("Changing receiver is not allowed")
+    }
+    
+    return store.update(id, newProps, cb);
+}
