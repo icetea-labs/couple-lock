a3caf3c28b219e8b7cc51ad85fb6a399efb48cea
Truong Hong Thi <thi1981@gmail.com>
2019-03-07 15:41:11 +0700

    Support advanced search

--- server/controllers/noti.js
+++ server/controllers/noti.js
@@ -1,4 +1,4 @@
 const Noti = require('../models/noti')
     , Controller = require('./controller')
 
-module.exports = new Controller(Noti).details().list('username').viewed().router
+module.exports = new Controller(Noti).details().list().viewed().router
